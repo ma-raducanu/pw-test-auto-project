30e779052330b90a385c2d913ac8581d9cfd76c4
Mircea-Alexandru Răducanu <mircea.alexandru.vi.raducanu@gmail.com>
2026-08-27 12:52:05 +0300

    Added some changes to folders

--- playwright.config.ts
+++ playwright.config.ts
@@ -14,7 +14,7 @@ dotenv.config();
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
-  testDir: './3-tests',
+  testDir: './tests',
   /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
